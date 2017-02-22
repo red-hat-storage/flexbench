ee73a17186ea0810254732198c717d265566587f
Andrew Ray <andrew@svds.com>
2017-02-22 17:40:00 -0600

    queries and basic readme

--- random_object_generation/README.md
+++ random_object_generation/README.md
@@ -1,3 +1,32 @@
 #Random Object Generation
 
-Note log-synth-0.1-SNAPSHOT-jar-with-dependencies.jar is built from https://github.com/tdunning/log-synth with patch from https://github.com/tdunning/log-synth/pull/31
+## How to run
+###Log generation
+To generate ~10TB of ~1GB log files joinable to TPC-DS SF 10000 (10k) run:
+```
+./generate.sh -sf=10k -m=10000 -c=10M -o=output_dir
+```
+Runtime will be ~3 min per mapper, so on a decent cluster with ~1000 map slots 30min.
+
+###Create table
+```
+hive -f create_table.sql \
+--hivevar table=part_log_test3 \
+--hivevar location=/user/andrew/part_log_test3 \
+--database default
+```
+
+###Enrichment query
+```
+hive -f enrichment.sql \
+--hivevar output_table=default.part_log_test3_enriched \
+--hivevar format=ORC \
+--hivevar log_table=default.part_log_test3 \
+--hivevar tpcdsdb=tpcds_bin_partitioned_orc_2 \
+--hivevar start_date=2017-02-21 \
+--hivevar end_date=2017-02-23
+```
+
+
+##Notes
+log-synth-0.1-SNAPSHOT-jar-with-dependencies.jar is built from https://github.com/tdunning/log-synth with patch from https://github.com/tdunning/log-synth/pull/31 that is required for use with hadoop-streaming.
---
+++ random_object_generation/enrichment.sql
@@ -0,0 +1,57 @@
+SET hive.exec.dynamic.partition.mode=nonstrict;
+
+-- set hivevar:output_table=default.part_log_test3_enriched;
+-- set hivevar:format=ORC;
+-- set hivevar:log_table=default.part_log_test3;
+-- set hivevar:tpcdsdb=tpcds_bin_partitioned_orc_2;
+-- set hivevar:start_date=2017-02-21;
+-- set hivevar:end_date=2017-02-23;
+ 
+-- testing only
+-- drop table ${output_table};
+
+
+CREATE TABLE IF NOT EXISTS ${output_table}(
+  ip STRING,
+  ts STRING,
+  tz STRING,
+  verb STRING,
+  resource_type STRING,
+  resource_fk INT,
+  response INT,
+  browser STRING,
+  os STRING,
+  customer INT,
+  d_day_name STRING,
+  i_current_price DECIMAL,
+  i_category STRING,
+  c_preferred_cust_flag STRING
+)
+PARTITIONED BY(ds STRING)
+STORED AS ${format};
+
+insert overwrite table ${output_table} partition (ds)
+select
+  ip,
+  ts,
+  tz,
+  verb,
+  resource_type,
+  resource_fk,
+  response,
+  browser,
+  os,
+  customer,
+  d_day_name,
+  i_current_price,
+  i_category,
+  c_preferred_cust_flag,
+  ds
+from ${log_table} log
+  join ${tpcdsdb}.item i on log.resource_fk = i.i_item_sk
+  join ${tpcdsdb}.customer c on log.customer = c.c_customer_sk
+  join ${tpcdsdb}.date_dim d on log.ds = d.d_date
+where
+  log.resource_type = 'item'
+  and log.ds between '${start_date}' and '${end_date}'
+;
