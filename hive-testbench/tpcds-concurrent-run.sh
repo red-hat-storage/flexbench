504147fde10326c80eb4bd9f9e218a1f547bdc4a
Paul Ho <paul@svds.com>
2017-08-10 17:22:12 -0700

    Make SCALE a parmeter for automating test runs.

--- hive-testbench/tpcds-concurrent-run.sh
+++ hive-testbench/tpcds-concurrent-run.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 QUERY_DIR=${1}
-CONCURRENCY=${2}
+
+# SCALE 1000 10000 100000
+SCALE=${2}
+
+CONCURRENCY=${3}
+
+# ENGINE_FORMAT "presto orc" "spark parquet" "hive orc" "hive-spark orc"
+ENGINE_FORMAT="spark parquet"
 
 for X in `seq 1 ${CONCURRENCY}`; do
-   echo "query_set,run_id,engine,format,scale_factor,query,status,start,end,tot_time,query_time,rows" > ${QUERY_DIR}/tpc_stats_${X}.log
+    echo "query_set,run_id,engine,format,scale_factor,query,status,start,end,tot_time,query_time,rows" > ${QUERY_DIR}/tpc_stats_${X}.log
 done
 
-# for SCALE in 1000 10000 100000 
-for SCALE in 1000; do
-   # for ENGINE_FORMAT in "presto orc" "spark parquet" "hive orc" "hive-spark orc" 
-   for ENGINE_FORMAT in "spark parquet"; do
-      for X in `seq 1 ${CONCURRENCY}`; do
-	  perl runSuite.pl ${QUERY_DIR} ${X} ${ENGINE_FORMAT} ${SCALE} &
-      done
-      wait
-   done
+for X in `seq 1 ${CONCURRENCY}`; do
+    perl runSuite.pl ${QUERY_DIR} ${X} ${ENGINE_FORMAT} ${SCALE} &
 done
+wait
 
 echo "Done!"
